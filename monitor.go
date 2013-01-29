e2b57a19dca37b094164095b12b85677921d681a
Travis Lane <travis.lane.osx@gmail.com>
2013-01-28 21:51:50 -0800

    Added a write function and hello world functionality

--- monitor.go
+++ monitor.go
@@ -3,6 +3,21 @@ package main
 import (
         "github.com/nsf/termbox-go"
        )
+       
+       
+func processInput(key termbox.Key) {
+
+}
+
+func write(x int, y int, mess string) {
+  for _, c := range mess {
+		termbox.SetCell(x, y, c, termbox.ColorDefault, termbox.ColorDefault)
+		x++
+	}
+}
+func clear() {
+  termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+}
         
 func main() {
   err := termbox.Init()
@@ -10,13 +25,16 @@ func main() {
 		panic(err)
 	}
 	defer termbox.Close()
-
+	write(1,1,"Hello, world")
+	termbox.Flush()
   loop:
 	  for {
 		  switch event := termbox.PollEvent(); event.Type {
 		  case termbox.EventKey:
 		    if event.Key == termbox.KeyCtrlQ {
 				  break loop
+			  } else {
+			    processInput(event.Key)
 			  }
 			  termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 			  termbox.Flush()
