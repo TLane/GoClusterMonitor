103240af53ba439afed383aa44ac86f299cef34f
Travis Lane <travis.lane.osx@gmail.com>
2013-01-29 23:27:44 -0800

    Added a box command

--- monitor.go
+++ monitor.go
@@ -1,10 +1,11 @@
 package main
 
+// termbox-go documentation http://godoc.org/github.com/nsf/termbox-go
+// Concurrency in go http://talks.golang.org/2012/concurrency.slide#1
 import (
         "github.com/nsf/termbox-go"
         "time"
        )
-       
        
 func processInput(key termbox.Key) {
 
@@ -16,15 +17,33 @@ func write(x int, y int, mess string) {
 		x++
 	}
 }
+
 func clear() {
   termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 }
+
 func flush() {
   termbox.Flush()
 }
+
 func centerMessage(mess string) {
   x,y := termbox.Size()
   write( ((x/2)-(len(mess)/2)) , ((y-1)/2), mess)
+}
+func box(v string, h string) {
+  x,y := termbox.Size()
+  for i :=0; i < x-1; i++ {
+    write(i,0,h)
+    write(i,y-1,h)
+  }
+  for i :=0; i < y; i++ {
+    write(0,i,v)
+    write(x-1,i,v)
+  }
+  write(x-1,y-1,"·")
+  write(0,0,"·")
+  write(0,y-1,"·")
+  write(x-1,0,"·")
 }
         
 func main() {
@@ -34,7 +53,8 @@ func main() {
 	}
 	defer termbox.Close()
 	write(1,1,"Hello, world")
-	termbox.Flush()
+	box("|","-")
+	flush()
   loop:
 	  for {
 		  switch event := termbox.PollEvent(); event.Type {
@@ -43,8 +63,12 @@ func main() {
 		      clear()
 		      centerMessage("Exiting...")
 		      flush()
-		      time.Sleep(2* time.Second)
+		      time.Sleep(1* time.Second)
 				  break loop
+			  } else if event.Key == termbox.KeyCtrlC {
+			    clear()
+			    flush()
+			    break loop
 			  } else {
 			    processInput(event.Key)
 			  }
